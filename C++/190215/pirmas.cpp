7c43241936d0668849a642e62ee417c94b79ef11
Sarunas <sarunas.zube@gmail.com>
2019-03-01 15:29:21 +0200

    commit initial

---
+++ C++/190215/pirmas.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    for(int i=0;i<10;i=i+1) {
+                    cout<<"  "<<i;
+                                    }
+
+
+
+
+
+return 0;
+}
+
